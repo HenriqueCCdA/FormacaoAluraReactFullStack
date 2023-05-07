e46985627dcdb7904e5aa774f9e77643a828adde
Henrique Andrade <henrique.ccda@gmail.com>
2023-05-07 15:13:25 -0300

    Curso 1 aula 2

--- curso1/alura-books/src/componentes/Logo/index.js
+++ curso1/alura-books/src/componentes/Logo/index.js
@@ -1,13 +1,17 @@
-import  './estilo.css'
+import './estilo.css'
 import logo from '../../imagens/logo.svg';
 
 function Logo() {
-    return (
-      <div className="logo">
-        <img src={logo} alt='logo'></img>
-        <p><strong>Alura</strong>Books</p>
-      </div>
-    )
+  return (
+    <div className="logo">
+      <img
+        src={logo}
+        alt='logo'
+        className="logo-img"
+      ></img>
+      <p><strong>Alura</strong>Books</p>
+    </div>
+  )
 }
 
 export default Logo;
