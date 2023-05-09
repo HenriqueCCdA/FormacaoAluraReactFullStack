d90116403adeffbe0d06301758bd49bcacbbd434
Henrique Andrade <henrique.ccda@gmail.com>
2023-05-09 15:32:03 -0300

    Curo 1 Aula  3.3

--- curso1/alura-books/src/componentes/Header/index.js
+++ curso1/alura-books/src/componentes/Header/index.js
@@ -1,15 +1,23 @@
-import './estilo.css'
 import Logo from "../Logo"
 import IconesHeader from "../IconesHeader"
 import OpcoesHeader from "../OpcoesHeader"
+import styled from "styled-components"
+
+
+const HeaderContainer = styled.header`
+  justify-content: center;
+  background-color: #FFF;
+  display: flex;
+`
+
 
 function Header() {
   return (
-    <header className='App-header'>
+    <HeaderContainer>
       <Logo />
       <OpcoesHeader />
       <IconesHeader />
-    </header>
+    </HeaderContainer>
   )
 }
 
